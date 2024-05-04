8f3831b94cd173f359cefc8cd7863441ed093e96
Niboah <o.niboah@gmail.com>
2024-05-04 13:33:39 +0200

    map

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 import sys
 import sys
 import time
@@ -12,7 +13,12 @@ from src.utils.read_file import getCities
 from src.view.window import MainWindow
 
 
-
+def find_adjacent_position_with_zero(matrix, x, y):
+    for i in range(max(0, x - 1), min(x + 2, len(matrix))):
+        for j in range(max(0, y - 1), min(y + 2, len(matrix[0]))):
+            if (i, j) != (x, y) and matrix[i][j] == 0:
+                return (i, j)
+    return None
 
 def main():
 
@@ -23,14 +29,35 @@ def main():
     matrix = np.zeros((num_rows, num_cols), dtype=object)
     
     negative_numbers = [-i for i in range(1, len(unique_cities) + 1)]
-    matrix = np.random.randint(0, len(unique_cities), size=(num_rows, num_cols))
+    #matrix = np.random.randint(0, len(unique_cities), size=(num_rows, num_cols))
     # Colocar números únicos en posiciones aleatorias en la matriz
     np.random.seed(42)  # Para reproducibilidad
+
+    points = []
+
     city_indices = np.random.choice(num_rows * num_cols, size=len(unique_cities), replace=False)
     for idx, num in zip(city_indices, negative_numbers):
         row = idx // num_cols  # Calcular la fila
         col = idx % num_cols   # Calcular la columna
         matrix[row, col] = num
+        points.append((row,col))
+
+    vector=[(0,1),(1,0),(0,-1),(-1,0)]
+    zeros=num_rows*num_cols-17
+
+    while zeros>0:
+        for i in range (1,18):
+            coordenadas = np.argwhere(matrix==-i)
+            coordenadas2 = np.argwhere(matrix==i)
+            coordenadas=np.concatenate((coordenadas,coordenadas2))
+            for x,y in coordenadas:
+                for xdir,ydir in vector:
+                    nx=x+xdir
+                    ny=y+ydir
+                    if(nx>=0 and nx <num_rows and ny>=0 and ny <num_cols and matrix[nx,ny]==0):
+                        matrix[nx,ny]=i
+                        zeros-=1
+    
     
     ################################################################################
 
@@ -38,7 +65,6 @@ def main():
 
     app = QApplication(sys.argv)
     window = MainWindow(matrix,colors)
-    window.initialize()
     window.show()
 
     sys.exit(app.exec())
--- src/view/window.py
+++ src/view/window.py
@@ -1,7 +1,8 @@
 
 from PyQt6 import QtGui, QtWidgets
-from PyQt6.QtCore import Qt, QPropertyAnimation, QPoint, QEasingCurve, QRectF
-from PyQt6.QtWidgets import QMainWindow, QWidget
+from PyQt6.QtCore import Qt, QPropertyAnimation, QPoint, QEasingCurve, QRectF, QTimer
+from PyQt6.QtWidgets import QMainWindow, QWidget, QHBoxLayout, QVBoxLayout, QPushButton
+from PyQt6.QtGui import QPainter, QPen
 
 
 CELL_SIZE = 20
@@ -11,14 +12,51 @@ class MainWindow(QMainWindow):
     def __init__(self,matrix,colors):
         super().__init__()
         self.setWindowTitle("Random travel simulation")
-        self.setGeometry(100, 100, 1000, 850)
+        self.setGeometry(100, 100, 1000, 850)        
+
+        self.layout = QHBoxLayout()
+
         self.label = QtWidgets.QLabel()
-        self.setCentralWidget(self.label)
+        self.layout.addWidget(self.label)
+        
+        self.rightLayout = QVBoxLayout()
+        self.layout.addLayout(self.rightLayout)
+
+        self.genMapBotton =  QPushButton("GenMap")
+        self.genMapBotton.clicked.connect(self.genMap)
+        self.rightLayout.addWidget(self.genMapBotton)
+
+        self.startBotton =  QPushButton("Start")
+        self.startBotton.clicked.connect(self.start)
+        self.startBotton.setEnabled(False)
+        self.rightLayout.addWidget(self.startBotton)
+
+        widget = QWidget()
+        widget.setLayout(self.layout)
+        self.setCentralWidget(widget)
+        
         self.matrix = matrix
         self.colors = colors
         self.ySize = len(matrix) + 1
         self.xSize = len(matrix[0]) + 1
+
+        self.initialize()
+        self.drawMatrix()
+
+        self.child = QWidget(self)
+        self.child.setStyleSheet("background-color:red;border-radius:15px;")
+        self.child.resize(HALF_CELL_SIZE,HALF_CELL_SIZE )
+        self.child.setVisible(False)
+
         
+    def genMap(self):
+        self.drawCell()
+        self.drawMatrix()
+        self.startBotton.setEnabled(True)
+
+    def start(self):
+        self.drawLine(10,10,22,23)
+ 
     def initialize(self):
         canvas = QtGui.QPixmap(1000, 850)
         canvas.fill(Qt.GlobalColor.white)
@@ -41,21 +79,33 @@ class MainWindow(QMainWindow):
         painter = QtGui.QPainter(canvas)
         pen = QtGui.QPen()
     
-        for y, row in enumerate(self.matrix):
-            for x, value in enumerate(row):
-                if value > 0. and y < len(self.matrix):                   
+        for y in range(0,len(self.matrix)):
+            for x in range(0,len(self.matrix)):
+                value = self.matrix[y][x]
+                if value > 0 :  
+                    print(value,y,x,end=" ---")   
                     pen.setWidth(CELL_SIZE)
-                    painter.setPen(pen)
-                    color = QtGui.QColor.fromHsv(int(self.colors[value]), 105, 255, 255)
+                    
+                    color = QtGui.QColor.fromHsv(int(self.colors[value-1]), 105, 255, 255)
                     pen.setColor(color)
+                    painter.setPen(pen)
+                    
 
                     painter.drawPoint(((x+1)*CELL_SIZE)+HALF_CELL_SIZE, ((y+1)*CELL_SIZE)+HALF_CELL_SIZE)
 
                 elif value < 0:
                     pen.setWidth(HALF_CELL_SIZE)
-                    painter.setPen(pen)
-                    color = QtGui.QColor.fromHsv(int(self.colors[value]), 255, 255, 255)
+                    color = QtGui.QColor.fromHsv(int(self.colors[(value*-1)-1]), 255, 255, 255)
                     pen.setColor(color)
+                    painter.setPen(pen)
+                    
+                    font = QtGui.QFont()
+                    font.setFamily('Times')
+                    font.setBold(True)
+                    font.setPointSize(10)
+                    painter.setFont(font)
+
+                    painter.drawText(((x+1)*CELL_SIZE)+HALF_CELL_SIZE, ((y+1)*CELL_SIZE)+HALF_CELL_SIZE, 'aAA')
 
                     rectangle = QRectF(((x+1)*CELL_SIZE)+HALF_CELL_SIZE, ((y+1)*CELL_SIZE)+HALF_CELL_SIZE, int(HALF_CELL_SIZE/10), int(HALF_CELL_SIZE/10))
                     painter.drawEllipse(rectangle)
@@ -70,19 +120,12 @@ class MainWindow(QMainWindow):
         painter.end()
         self.label.setPixmap(canvas)
 
-        
-
-    def animate(self):
-        self.child = QWidget(self)
-        self.child.setStyleSheet("background-color:red;border-radius:15px;")
-        self.child.resize(10, 10)
+        self.child.setVisible(True)
         self.anim = QPropertyAnimation(self.child, b"pos")
-        self.anim.setEasingCurve(QEasingCurve.Type.InOutCubic)
-        self.anim.setStartValue(QPoint(100,0))
-        self.anim.setEndValue(QPoint(400, 400))
-        self.anim.setDuration(15000)
+        self.anim.setStartValue(QPoint(((iX+1)*CELL_SIZE)+HALF_CELL_SIZE,((iY+1)*CELL_SIZE)+HALF_CELL_SIZE))
+        self.anim.setEndValue(QPoint(((dX+1)*CELL_SIZE)+HALF_CELL_SIZE, ((dY+1)*CELL_SIZE)+HALF_CELL_SIZE))
+        self.anim.setDuration(1500)
         self.anim.start()
-        
 
     def drawText(self,text,x,y):
         canvas = self.label.pixmap()
@@ -108,4 +151,3 @@ class MainWindow(QMainWindow):
         self.initialize()
         self.drawCell()
         self.drawMatrix()
-        
